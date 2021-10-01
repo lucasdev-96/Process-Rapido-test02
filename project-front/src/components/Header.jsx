2c8704ea066f0c134b94e296085e9f6baa5a3b9c
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-10-01 13:02:59 -0300

    Adicionando menu hamburguer

--- project-front/src/components/Header.jsx
+++ project-front/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bubble as Menu } from 'react-burger-menu';
 import PropTypes from 'prop-types';
 import { BiBarChartAlt } from 'react-icons/bi';
 import { IoLogoReddit } from 'react-icons/io';
@@ -6,18 +7,29 @@ import { AiOutlineShoppingCart, AiOutlineSearch } from 'react-icons/ai';
 import '../styles/header.css';
 
 function Header({ width }) {
+  const links = () => (
+    <>
+      <a href="#home">HOME</a>
+      <a href="#home">PAGES</a>
+      <a href="#home">PORTFOLIOS</a>
+      <a href="#home">HEADERS</a>
+      <a href="#home">ELEMENTS</a>
+      <a href="#home">BLOGS</a>
+    </>
+  );
+
+  const linksMobile = () => (
+    <Menu className="btn_burguer" right>
+      {links()}
+    </Menu>
+  );
+
   return (
     <header>
       <div className="header">
-        <IoLogoReddit style={{ fontSize: '50px' }} className="icons" />
+        {width <= 768 ? null : <IoLogoReddit style={{ fontSize: '50px' }} className="icons" />}
         <nav className="links">
-          <a href="#home">HOME</a>
-          <a href="#home">PAGES</a>
-          <a href="#home">PORTFOLIOS</a>
-          <a href="#home">HEADERS</a>
-          <a href="#home">ELEMENTS</a>
-          <a href="#home">BLOGS</a>
-
+          {width <= 768 ? linksMobile() : links()}
         </nav>
         { width <= 768 ? null : (
           <div>
